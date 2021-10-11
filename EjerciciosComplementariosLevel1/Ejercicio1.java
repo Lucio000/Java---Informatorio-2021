de7e5bfd6ec006b0f02652d27de32e32ca23e448
Lucio <luciorodiaz@gmail.com>
2021-10-11 12:42:36 -0300

    se realizo el ejercicio 1

---
+++ EjerciciosComplementariosLevel1/Ejercicio1.java
@@ -0,0 +1,13 @@
+package EjerciciosComplementariosLevel1;
+
+
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        /* Realizar un programa que realice la impresión por consola del siguiente mensaje "Hola Informatorio 2020!!" */
+
+        System.out.println("Hola Informatorio 2020!!");
+    }
+}
