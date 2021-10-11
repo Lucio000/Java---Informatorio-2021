37de22bf75c8445821989bb153ae1fea61d35f9f
Lucio <luciorodiaz@gmail.com>
2021-10-11 13:51:04 -0300

    Se realiza el ejercicio 4

---
+++ EjerciciosComplementariosLevel1/Ejercicio4.java
@@ -0,0 +1,36 @@
+package EjerciciosComplementariosLevel1;
+import java.util.*;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+
+        /* 
+        Realizar un programa que calcule el factorial de un número: n! = 1 x 2 x 3 x 4 x 5 x … x (n-1) x n.
+        Sin hacer uso de librerías.
+        */
+
+        // 
+        System.out.println(" ");
+
+        // 
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int c = 1;
+
+        for ( int i = 1; i <= a; i++){
+
+            c = c *i;
+        }
+        
+        System.out.println("El factorial de "+ a + " es : " +c);
+    
+        scan.close();
+
+
+
+
+    }      
+    
+}
