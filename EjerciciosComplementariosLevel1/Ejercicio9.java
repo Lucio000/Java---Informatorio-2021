c012fd40bb78d3fb6a43194a23f07a9abfc75959
Lucio <luciorodiaz@gmail.com>
2021-10-11 15:27:39 -0300

    se agrega comentarios

---
+++ EjerciciosComplementariosLevel1/Ejercicio9.java
@@ -0,0 +1,14 @@
+package EjerciciosComplementariosLevel1;
+
+
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+
+        /* 
+        Dado un String de entrada (frase, texto, etc) calcular 
+        la cantidad de veces que aparece una letra dada.
+        */
+
+    }
+}
